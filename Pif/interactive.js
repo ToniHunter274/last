25090c496ded669bf5d875b6f41f077e939e141e
Toni Hunter <Toniprime15@gmail.com>
2022-04-11 01:50:15 -0230

    iswear

---
+++ Pif/interactive.js
@@ -0,0 +1,24 @@
+$("#repa1").on("click", function(){
+	$("#repli1").css("display","flex")
+
+});
+$("#repa2").on("click", function(){
+	$("#repli2").css("display","flex")
+
+});
+$("#repa3").on("click", function(){
+	$("#repli3").css("display","flex")
+
+});
+
+$("#etext").on("click",function(){
+	$("#myCon").attr("contenteditable", "true");
+	$("#update").css("display", "flex")
+});
+
+
+
+
+
+
+
